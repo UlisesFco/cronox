ec326e4618d6f56499eb29f542fa72d38cdb468a
Ulises Francisco Alejandre Navarro <ulisesfcoan@gmail.com>
2022-08-26 19:13:31 -0500

    Primera versión terminada de cronox
    Se subió la primera versión de la extensión, la cual tiene:
    - Funcionalidad básica
    - Íconos de extensión
    - Texto en español, inglés y portugués

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# cronox
-Un cronómetro simple, realizado como extensión de navegador
+# Cronox
+
+## ES
+
+Un cronómetro simple, realizado como extensión de navegador.
+
+La extensión tiene cuatro botones:
+
+- Iniciar: empieza la cuenta del tiempo.
+- Pausar: pausa la cuenta del tiempo transcurrido.
+- Continuar: continua la cuenta del tiempo, solo funciona si se ha pausado el tiempo anteriormente.
+- Reiniciar: reinicia la cuenta del tiempo (pone el reloj en 00:00:00).
+
+![Captura de pantalla de la extensión](./doc/cronox-es.png "Diseño de la extensión")
+
+<!-- Disponible en:
+
+- ![Link a la Chrome Web Store](github.com "Chrome")  
+- ![Link a Firefox Browser Add-Ons](github.com "Firefox") -->
+
+## EN
+
+A simple chronometer, coded as a browser extension.
+
+The extension has four buttons:
+
+- Start: starts counting up time.
+- Pause: pauses the elapsed time counting process.
+- Continue: continues to count up time, only works if the pause button was pressed beforehand.
+- Restart: restarts the chronometer (puts the clock back to 00:00:00).
+
+![Extension screenshot](./doc/cronox-en.png "Extension's design")
+
+<!-- Available in:
+
+- ![Link a la Chrome Web Store](github.com "Chrome")  
+- ![Link a Firefox Browser Add-Ons](github.com "Firefox") -->
+
+## PT
+
+Um cronômetro simples, desenvolvido como uma extensão de navegador.
+
+A extensão tem quatro botões:
+
+- Inicializar: começa a contagem do tempo.
+- Pausar: pausa a contagem de tempo decorrido.
+- Continuar: continua a contagem de tempo, só funciona se o botão de pausa foi pressionado previamente.
+- Reinicializar: redefine o cronômetro (põe o relógio em 00:00:00).
+
+![Extension screenshot](./doc/cronox-pt.png "Extension's design")
+
+<!-- Disponível em:
+
+- ![Link à Chrome Web Store](github.com "Chrome")  
+- ![Link a Firefox Browser Add-Ons](github.com "Firefox") -->
---
+++ popups/cronox.js
@@ -0,0 +1,145 @@
+/**
+ * Este archivo es JavaScript es parte de la extensión «Cronox».
+ *
+ * @author      Ulises Francisco Alejandre Navarro
+ * @since       2022-08-26
+ * @license     MPL 2.0
+ *
+ * Este codigo fuente está sujeto a los términos de la Licencia Pública
+ * de Mozilla, v. 2.0. Si una copia de la licencia no fue distribuída con
+ * este archivo, la puede encontrar en http://mozilla.org/MPL/2.0/.
+ */
+
+'use strict';
+
+if (typeof browser === "undefined") {
+    var browser = chrome;
+} else {
+    var browser = browser || chrome
+}
+
+localizarPagHtml();
+
+var corriendo = false;
+var iniciado = false;
+var tiempo = 0; // en centésimas de segundo
+var reloj = 0; // Variable que contine el timer que se actualiza cada 10 ms
+
+// Localizar el reloj de la aplicación
+var cronometro = document.getElementById("reloj");
+
+/**
+ * Asignarle a cada botón la función indicada
+ */ 
+document.getElementById("b_iniciar").addEventListener("click", iniciarCrono);
+document.getElementById("b_pausar").addEventListener("click", pausarCrono);
+document.getElementById("b_continuar").addEventListener("click", continuarCrono);
+document.getElementById("b_reiniciar").addEventListener("click", reiniciarCrono);
+    	
+// Funciones para el funcionamiento del cronómetro
+/**
+ * Modifica el contador del cronómetro a partir del tiempo transcurrido.
+ */
+function calcularTiempo() {
+    tiempo++;
+
+    let mins = Math.floor(tiempo/6000);
+    let resMin = tiempo - mins*6000;
+    let seg = Math.floor(resMin/100);
+    let cs = Math.floor((resMin - seg*100));
+    
+    // Siempre poner los numeros con dos dígitos
+    if(cs < 10) {cs = "0" + cs;}
+    if(seg < 10) {seg = "0" + seg;}
+    if(mins < 10) {mins = "0" + mins;}
+
+    cronometro.innerHTML = mins + ":" + seg + ":" + cs;
+}
+
+/**
+ * Empieza la cuenta del cronómetro.
+ */
+function iniciarCrono() {
+    if (!corriendo && !iniciado) {
+        corriendo = true;
+        iniciado = true;
+        reloj = setInterval(calcularTiempo, 10)
+    }
+}
+
+/**
+ * Pausa la cuenta del cronómetro.
+ */
+function pausarCrono() {
+    if(corriendo) {
+        corriendo = false;
+        clearInterval(reloj);
+    }
+
+}
+
+/**
+ * Continúa con la cuenta pausada del cronómetro.
+ */
+function continuarCrono() {
+    if (iniciado) {
+        iniciado = false;
+        iniciarCrono();
+    }
+}
+
+/**
+ * Regresa la cuenta del cronómetro a 0.
+ */
+function reiniciarCrono() {
+    if (iniciado){
+        if (corriendo) {
+            corriendo = false;
+            clearInterval(reloj);
+        }
+        tiempo = 0;
+        iniciado = false;
+        cronometro.innerHTML = "00:00:00"
+    }
+}
+
+// Funciones para la localización de la extensión
+/**
+ * Busca los elementos HTML localizables y pone el mensaje correspondiente en su lugar
+ */    
+function localizarPagHtml() {
+    // Localizar usando los tags __MSG_***__ de data-localize
+    // Juntar todos los elementos html con el tag data-localize
+    var elementos = document.querySelectorAll('[data-localize]');
+    
+    // Reemplazar el texto por defecto con los mensajes correspondientes
+    // a la localización
+    for (var i in elementos){ 
+        // verificar que se tenga la propiedad data-localize
+        if (elementos.hasOwnProperty(i)) {
+            let elem = elementos[i];
+            let tag = elem.getAttribute('data-localize').toString();
+    
+            reemplazar(elem, tag);
+        }
+    }
+}
+
+// Funciones para la localización de la extensión
+/**
+ * Reemplaza los tags __MSG_***__ con el mensaje de localización correspondiente
+ * @param {*} obj: elemento html que se quiere localizar
+ * @param {*} tag: contenido del tag data-localize de cada objeto html anterior
+ */
+ function reemplazar(objeto, tag) {
+    // Reemplazar los tags con MSG
+    var mensaje = tag.replace(/__MSG_(\w+)__/g, function(match, v1) {
+        return v1 ? browser.i18n.getMessage(v1) : '';
+    });
+        
+    // Reemplazar el contenido del tag con el mensaje del json correspondiente
+    if(mensaje != tag) {
+        objeto.innerHTML = mensaje;
+    }
+}
+
